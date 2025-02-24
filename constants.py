d064e49d9cd69f700700eb501de763152cd38572
Abdelhady Sayed Abdelhady <Abdelhadysayed_p@sci.asu.edu.eg>
2025-02-25 00:43:21 +0200

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,7 +1,7 @@
 # Twilio credentials (replace with your own)
-# ACCOUNT_SID = "Your TWILIO ACOUNT SID"
-# AUTH_TOKEN = "Your TWILIO AUTH TOKEN"
-# TWILIO_PHONE_NUMBER = "Your TWILIO Number"  # Must be a Twilio number
+ACCOUNT_SID = "Your TWILIO ACOUNT SID"
+AUTH_TOKEN = "Your TWILIO AUTH TOKEN"
+TWILIO_PHONE_NUMBER = "Your TWILIO Number"  # Must be a Twilio number
 
 # Hardcoded default message
 DEFAULT_MESSAGE = "No message provided. This is a hardcoded message."
